15ff40b3d979ec07a1279533c0c715f208ba10f6
Walter Souto <wsouto@gmail.com>
2025-12-12 19:47:13 -0300

    Add srcDir configuration to point to client directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  srcDir: "./src/client",
   adapter: cloudflare({
     platformProxy: {
       enabled: true
